320ac5f52cbc256e714cb7ddd897e1fea4391fb3
Captain-5415 <gowtham996644@gmail.com>
2026-05-15 19:35:02 +0530

    First commit

---
+++ script.js
@@ -0,0 +1,222 @@
+// JavaScript Tasks - 50 Questions
+// Topic : Variables + Printing Statements
+
+
+// // 1. Create a variable using var and store your name.
+// var name = "Gowtham";
+
+// // 2. Print your name using console.log().
+// console.log(name);
+
+// // 3. Create a variable using let and store your age.
+// let age = 23;
+
+// // 4. Change the value of age and print it.
+// age = 16;
+// console.log(age);
+
+// // 5. Create a constant variable for country name.
+// const country = "India"
+
+// // 6. Print the country name.
+// console.log(country)
+
+// // 7. Try redeclaring var variable and observe output.
+// var name = "Luffy"      //we can re-declare & re-initialize var variable
+// console.log(name)
+
+// 8. Try redeclaring let variable and observe error.
+// let age = 49
+        // (we cant re-declare let variable)
+        // (It shows syntax error cuz we already declared "age")
+
+// 9. Try reassigning const variable and observe error.
+// const country = "US"
+        // (we cant re-declare const variable)
+        // (It shows syntax error cuz we already declared "country")
+
+// 10. Write difference between var and let.
+    // var
+        //declaration, initilization, re-use, re-initilization, re-declaration
+    // let
+        //declaration, initilization, re-use, re-initilization
+    //var is function scope and let is block scope
+    //In var we can redeclare the variable with same name but this is not possible with let variable
+
+// 11. Write difference between let and const.
+    // let
+        //declared first and initilize the value later
+        //value can be changed
+    // const
+        //must initilize the values while declaration
+        //value cannot be changed (fixed value)
+
+// // 12. Predict output
+// var a = 10
+// a = 20
+// var a = 30
+// console.log(a)  //output :- 30
+
+// // 13. Predict output
+// let b = 50
+// b = 60
+// console.log(b)  //output :- 60
+
+// // 14. Predict output
+// const c = 100
+// console.log(c)  //output :- 100
+
+// // 15. Use console.log() to print "Hello World".
+// console.log("Hello World.")
+
+// // 16. Use alert() to show "Welcome User".
+// alert("Welcome User")
+
+// // 17. Use confirm() to ask user:
+// // "Do you like JavaScript?"
+// confirm("Do you like JavaScript?")
+
+// // 18. Use prompt() to ask user name.
+// var UserName = prompt("Enter Your Name")
+// console.log(UserName)
+
+// // 20. Use document.writeln() to print "Batch 33".
+// document.writeln("Batch 33")
+
+// // 21. Create variable studentName and store your name.
+// var studentName = "Gowtham Goriparthi"
+
+// // 22. Create variable studentAge and store your age.
+// var studentAge = 23
+
+// // 23. Print both variables together.
+// console.log("studentName: "+studentName+", studentAge:"+studentAge)
+
+// // 24. Create a variable for mobile number.
+// var mobile= 9052520552
+
+// // 25. Print mobile number using console.log().
+// console.log(mobile)
+
+// // 26. Create variable using var for favorite color.
+// var favorite_Color = "Blue"
+
+// // 27. Reassign another color value.
+// favorite_Color = "Red"
+// console.log(favorite_Color)
+
+// 28. Create variable using let for marks.
+let marks = 100
+
+// 29. Change marks value and print it.
+marks = 99
+console.log(marks)
+
+// 30. Create const variable for PI value.
+const Pi_value = Math.PI
+
+// 31. Print PI value.
+console.log(Pi_value)
+
+// 32. Predict output
+var x = 5
+var x = 15
+console.log(x)  //output:- 15
+
+// 33. Predict output
+let y = 25
+y = 35
+console.log(y)  //output:- 35
+
+// 34. Find error
+// const z = 40
+// z = 45
+// console.log(z)  //TypeError, cuz const value cant be changed
+
+// 35. Create alert box with your college name.
+// alert("Usha Rama College of Engineering and Technology")
+
+// 36. Ask user age using prompt().
+// var UserAge = prompt("Enter Your Age")
+
+// 37. Print user age in console.
+// console.log("User Age: "+UserAge)
+
+// 38. Ask user confirmation:
+// "Did you complete HTML?"
+// var result = confirm("Did you complete HTML?")
+
+// 39. Print confirm result in console.
+// if(result){
+//     console.log("Completed")
+// }
+// else{
+//     console.log("Not Completed")
+// }
+
+// 40. Print "JavaScript Class" using document.writeln().
+document.writeln("JavaScript Class")
+
+// 41. Create 3 variables:
+// city, state, country.
+var city = "Vijayawada"
+var state = "Andhra Pradesh"
+var country = "India"
+
+// 42. Print all variables together.
+console.log(city,state,country)
+
+// 43. Create one example for var.
+var a = 10
+a = 20
+var a = 30
+console.log(a)
+
+// 44. Create one example for let.
+let b = 101
+b = 102
+// let b = 105 (we cant redeclare the variable using let)
+console.log(b)
+
+
+// 45. Create one example for const.
+const c = "fixed"
+// c = 10   (we cant reasign values to const variable)
+// const c = 99 (we cant redeclare variable using const)
+console.log(c)
+
+// 46. Predict output
+var num = 100
+num = 200
+console.log(num)    //output:- 200
+
+
+// 47. Predict output
+let price = 500
+price = 700
+console.log(price)  //output:- 700
+
+
+// 48. Predict output
+const password = 12345
+console.log(password)   //output:- 12345
+
+
+// 49. Write rules of var.
+// var
+    //It used to create variables (declaration and initilization0)
+    //values can be redeclare and reinitilize
+    //It is function scope
+
+
+// 50. Write rules of let and const.
+// let
+    //It used to create variables (declaration and initilization0)
+    //Variables cannot be redeclare
+    //Variables can be reinitilize
+    //It is block scope
+//const
+    //It used to create fixed variables (declaration and initilization0)
+    //Variables cannot be redeclare
+    //Variables canot be reinitilize
+    //It is block scope
